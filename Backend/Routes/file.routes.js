3742741f4178034593099332fc6381a2c3504b80
Mohd Saif Khan <msaifkhan5038@gmail.com>
2023-11-26 20:10:56 +0530

    Time and Date Update Base on Indian Time

--- Backend/Routes/file.routes.js
+++ Backend/Routes/file.routes.js
@@ -1,135 +1,150 @@
-
-const express = require('express');
-const multer = require('multer');
-const fs=require("fs")
-const {Filedata}=require("../Models/File.model")
-const { auth } = require('../Middleware/auth');
-const {s3}=require("../Config/S3_Config");
-const { RoleBase } = require('../Middleware/RoleBase');
-require("dotenv").config()
-const fileRouter = express.Router()
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const { Filedata } = require("../Models/File.model");
+const { auth } = require("../Middleware/auth");
+const { s3 } = require("../Config/S3_Config");
+const { RoleBase } = require("../Middleware/RoleBase");
+require("dotenv").config();
+const fileRouter = express.Router();
 
 const bucketName = process.env.S3bucketName;
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: "uploads/" });
 
-fileRouter.post('/upload', upload.single('file'),auth,RoleBase(["user", "admin"]), async (req, res) => {
-
+fileRouter.post(
+  "/upload",
+  upload.single("file"),
+  auth,
+  RoleBase(["user", "admin"]),
+  async (req, res) => {
     const { file } = req;
 
     // Reading the file from the local filesystem
     const fileContent = fs.readFileSync(file.path);
-  
+
     // Specifying the parameters for S3 upload
     const params = {
       Bucket: bucketName,
-      Key: `${file.originalname}`, 
+      Key: `${file.originalname}`,
       Body: fileContent,
     };
-  
+
     // Uploading the file to S3
-    
-    s3.upload(params, async(err, data) => {
+
+    s3.upload(params, async (err, data) => {
       if (err) {
-        console.error('Error uploading file to S3:', err);
-    
-            res.status(500).send('Internal Server Error');
-        } else {
-          console.log('File uploaded successfully. S3 URL:', data.Location);
-          const imageurlS3=data.Location
+        console.error("Error uploading file to S3:", err);
 
-          try {
+        res.status(500).send("Internal Server Error");
+      } else {
+        console.log("File uploaded successfully. S3 URL:", data.Location);
+        const imageurlS3 = data.Location;
 
-            const userId = req.body.user;
-            // Add file metadata to MongoDB
-            const metadata = new Filedata({
-              filename: imageurlS3,
-              uploadDate: new Date(),
-              user: userId,
-            });
-            await metadata.save();
+        try {
+          const userId = req.body.user;
 
-            res.status(201).send('File uploaded successfully!');
-        
-            // res.json({ message: 'File uploaded successfully!' });
-          } catch (error) {
-            console.error('Error uploading file:', error);
-            res.status(500).send({ error: 'Error uploading file.' });
-          }
+
+          // for time and Date
+          const options = {
+            timeZone: 'Asia/Kolkata', // Seting the time zone to Indian Standard Time
+            hour12: false, // Using 24-hour clock
+            day: '2-digit', // Displaying day as two digits
+            month: '2-digit', // Displaying month as two digits
+            year: 'numeric', // Displaying year in numeric format
+            hour: '2-digit', // Displaying hour as two digits
+            minute: '2-digit', // Displaying minute as two digits
+            second: '2-digit', // Displaying second as two digits
+          };
+          
+          const indianDateAndTime = new Date().toLocaleString('en-IN', options);
+          
+          
+          
+
+          // Add file metadata to MongoDB
+          const metadata = new Filedata({
+            filename: imageurlS3,
+            uploadDate: indianDateAndTime,
+            user: userId,
+          });
+          await metadata.save();
+
+          res.status(201).send("File uploaded successfully!");
+
+          // res.json({ message: 'File uploaded successfully!' });
+        } catch (error) {
+          console.error("Error uploading file:", error);
+          res.status(500).send({ error: "Error uploading file." });
         }
-      });
+      }
+    });
+  }
+);
 
-});
-
-
-fileRouter.get('/files',auth,RoleBase(["admin"]), async (req, res) => {
+fileRouter.get("/files", auth, RoleBase(["admin"]), async (req, res) => {
   // getting the list of uploaded files from MongoDB
   try {
     const files = await Filedata.find();
 
     // console.log(files)
 
-    let PopulatedFiles=await Filedata.populate(files, { path: "user" });
+    let PopulatedFiles = await Filedata.populate(files, { path: "user" });
 
-   
     res.status(200).send(PopulatedFiles);
-
   } catch (error) {
-    console.error('Error fetching files:', error);
-    res.status(500).send({ error: 'Error fetching files.' });
+    console.error("Error fetching files:", error);
+    res.status(500).send({ error: "Error fetching files." });
   }
 });
 
+fileRouter.get("/files/:userId", auth, RoleBase(["user"]), async (req, res) => {
+  try {
+    const userId = req.params.userId;
 
+    // Retrieve the list of uploaded files from MongoDB for a specific user
+    const files = await Filedata.find({ user: userId }).populate("user");
 
-fileRouter.get('/files/:userId',auth,RoleBase(["user"]), async (req, res) => {
-    try {
-      const userId = req.params.userId;
-  
-      // Retrieve the list of uploaded files from MongoDB for a specific user
-      const files = await Filedata.find({ user: userId }).populate("user");
-     
-     res.status(200).send(files);
-
-    } catch (error) {
-      
-      res.status(500).send({ error: 'Error fetching files.' });
-    }
-  });
-  fileRouter.delete('/delete/:filename',auth,RoleBase(["user", "admin"]), async (req, res) => {
+    res.status(200).send(files);
+  } catch (error) {
+    res.status(500).send({ error: "Error fetching files." });
+  }
+});
+fileRouter.delete(
+  "/delete/:filename",
+  auth,
+  RoleBase(["user", "admin"]),
+  async (req, res) => {
     const filename = req.params.filename;
-    console.log('Deleting file:', filename);
-  
+    console.log("Deleting file:", filename);
+
     // Delete file from S3
     const deleteParams = {
       Bucket: bucketName,
       Key: filename,
     };
-    console.log('Delete Params:', deleteParams);
-  
+    console.log("Delete Params:", deleteParams);
+
     try {
-        // Removing file metadata from S3
+      // Removing file metadata from S3
       const response = await s3.deleteObject(deleteParams).promise();
-      console.log("yhdvbc",response)
+      console.log("yhdvbc", response);
 
       if (response && response.$metadata) {
-        console.error('Error deleting file:', response);
+        console.error("Error deleting file:", response);
         // Handle the error as needed
       } else {
         // File deleted successfully
         console.log('"1File deleted successfully!');
-          // Removing file metadata from MongoDB
-      await Filedata.deleteOne({ filename: filename });
-        
-       
-       res.status(204).send({ message: 'File deleted successfully!' });
+        // Removing file metadata from MongoDB
+        await Filedata.deleteOne({ filename: filename });
 
+        res.status(204).send({ message: "File deleted successfully!" });
       }
-      
     } catch (error) {
-      console.error('Error deleting file:', error);
-      res.status(500).send({ error: 'Error deleting file.' });
+      console.error("Error deleting file:", error);
+      res.status(500).send({ error: "Error deleting file." });
     }
-  });
+  }
+);
 
-
-module.exports={fileRouter}
+module.exports = { fileRouter };
--- my-file-upload-app/src/components/upload.jsx
+++ my-file-upload-app/src/components/upload.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
-import '../FileCSS/File.css'; 
+import "../FileCSS/File.css";
 
 const FileUpload = () => {
   const [userID, setUserID] = useState(localStorage.getItem("userID"));
@@ -19,7 +19,7 @@ const FileUpload = () => {
     // let BaseUrl=`http://localhost:3001`
 
     // this is latest Deploye Link which is below
-    let BaseUrl=`https://filedrive-management.onrender.com`
+    let BaseUrl = `https://filedrive-management.onrender.com`;
 
     try {
       const response = await fetch(`${BaseUrl}/file/upload`, {
@@ -32,36 +32,34 @@ const FileUpload = () => {
 
       // Checking if the response status is OK (200)
       if (response.ok) {
-        
         const responseText = await response.text();
 
         if (responseText === "File uploaded successfully!") {
           // Updating the file list after a successful upload
           console.log("file Uploaded");
           // Swal.fire(responseText)
-          console.log(response)
+          console.log(response);
 
           Swal.fire({
             title: responseText,
-        
-            icon: 'success',
-            
+
+            icon: "success",
           });
           updateFileList();
         }
       } else {
         // Handle non-OK response (e.g., redirect, different content type)
         Swal.fire({
-          title: 'Something went wrong',
-          icon: 'error',
+          title: "Something went wrong",
+          icon: "error",
         });
         console.error("Error uploading file. Status:", response.status);
       }
     } catch (error) {
       // Handle fetch errors
       Swal.fire({
-        title: 'Something went wrong',
-        icon: 'error',
+        title: "Something went wrong",
+        icon: "error",
       });
       console.error("Error uploading file:", error);
     }
@@ -75,7 +73,7 @@ const FileUpload = () => {
       // let BaseUrl=`http://localhost:3001`
 
       // this is latest Deploye Link which is below
-      let BaseUrl=`https://filedrive-management.onrender.com`
+      let BaseUrl = `https://filedrive-management.onrender.com`;
       const response = await fetch(
         `${BaseUrl}/file/delete/${encodeURIComponent(filename)}`,
         {
@@ -100,15 +98,13 @@ const FileUpload = () => {
     }
   };
 
-
-
   const updateFileList = async () => {
     try {
       // let BaseUrl=`https://filemanagement-ikvm.onrender.com`
       // let BaseUrl=`http://localhost:3001`
 
       // this is latest Deploye Link which is below
-      let BaseUrl=`https://filedrive-management.onrender.com`
+      let BaseUrl = `https://filedrive-management.onrender.com`;
       // Use the appropriate API endpoint based on user role
       const apiEndpoint =
         userRole === "admin"
@@ -126,7 +122,7 @@ const FileUpload = () => {
 
       if (Array.isArray(files)) {
         setFileList(files);
-        console.log("hfhjj",files)
+        console.log("hfhjj", files);
       } else {
         console.error("Invalid response format. Expected an array:", files);
       }
@@ -152,18 +148,23 @@ const FileUpload = () => {
           ref={(input) => setFileInput(input)}
         />
         <br />
-       
+
         <br />
         <button type="submit">Upload File</button>
       </form>
 
       <hr />
 
-      <h2>{userRole === "admin" ? "All Files Created by All Users" : "User Files Created By you"}</h2>
+      <h2>
+        {userRole === "admin"
+          ? "All Files Created by All Users"
+          : "User Files Created By you"}
+      </h2>
       <table>
         <thead>
           <tr>
             <th>File Name</th>
+            <th>User Information</th>
             <th>Actions</th>
           </tr>
         </thead>
@@ -171,7 +172,8 @@ const FileUpload = () => {
           {fileList.map((file) => (
             <tr key={file._id}>
               <td>
-                {file.filename.endsWith(".jpg") || file.filename.endsWith(".png") ? (
+                {file.filename.endsWith(".jpg") ||
+                file.filename.endsWith(".png") ? (
                   <img
                     src={file.filename}
                     alt={file.filename}
@@ -180,6 +182,18 @@ const FileUpload = () => {
                 ) : (
                   file.filename
                 )}
+              </td>
+              <td>
+                <div>
+                  <p>Name: {file.user ? file.user.name : "Unknown User"}</p>
+                  <p>Role: {file.user ? file.user.role : "Unknown Role"}</p>
+                  <p>
+                    Contact No:{" "}
+                    {file.user ? file.user.mobile_No : "Unknown Contact No"}
+                  </p>
+                  <p>Email: {file.user ? file.user.email : "Unknown Email"}</p>
+                  <p>Upload Date: {file.uploadDate}</p>
+                </div>
               </td>
               <td>
                 <button>
